07c6096d9deb9729ba5ff687ceb3ec362ed94aa9
mnyerczan <mnyerczan@outlook.hu>
2020-11-08 13:53:21 +0100

    test method increased

--- Test/Controllers/settingsTest.php
+++ Test/Controllers/settingsTest.php
@@ -1,33 +1,46 @@
+#!/usr/bin/php7.4
 <?php 
 
+use App\Controller\Main\Settings\SettingsAccountController;
+
+chdir("/var/www/html/NBack");
+
 require 'Test/init.php';
-require APPLICATION.'Controllers/SettingsController.php';
+require APPLICATION.'Controller/Main/Settings/SettingsAccountController.php';
 
 
-$sC = new SettingsController();
 /*
+ | ----------------------------------------------------------
+ | A SettingsAccountController ostrély validatePass metódusa
+ | belső tagváltozókba ír, ezért megtartja az állapotát.
+ | Minden tezstnél új objektumot kell inicializálni!!
+ | ----------------------------------------------------------
+ */
+class T extends SettingsAccountController
+{
+    public function __construct(){}
+}
+
+
+
 // Password change test.
 // Bad old password
-$d['update-user-old-password'] = 'xyzv..';
-$d['update-user-password'] = 'almafa';
-$d['update-user-retype-password'] = 'almafa';
-print_r($sC->validatePass($d));
+test(new T(), "validatePass", ['xyzv..', 'almafa','almafa'], true);
 
-// Different new password
-$d['update-user-old-password'] = 'almafa';
-$d['update-user-password'] = 'csokibeka';
-$d['update-user-retype-password'] = 'csokibe';
-print_r($sC->validatePass($d));
+// Different new password;
+test(new T(), "validatePass", ["almafák","csokibekak","csokibeka"],  false);
 
 // Too short password
-$d['update-user-old-password'] = 'almafa';
-$d['update-user-password'] = 'cso';
-$d['update-user-retype-password'] = 'cso';
-print_r($sC->validatePass($d));
+test(new T(), "validatePass", ["almafa","cso","cso"], false);
 
 // Good passwords
-$d['update-user-old-password'] = 'almafa';
-$d['update-user-password'] = 'csokibeka';
-$d['update-user-retype-password'] = 'csokibeka';
-print_r($sC->validatePass($d));
-*/
+test(new T(), "validatePass", ["almafak","csokibekakak","csokibekakak"], true);
+
+// számokkal
+test(new T(), "validatePass", ["almafa", "csokibeka1","csokibeka1"],  true);
+
+// különleges karakterekkel
+test(new T(), "validatePass", ["almafa_", "csokibeka?","csokibeka?"], true);
+
+// különleges karakterekkel
+test(new T(), "validatePass", ["almafa./?", "cs@kibekák1?","cs@kibekák1?"], true);
--- Test/init.php
+++ Test/init.php
@@ -1,6 +1,7 @@
+#!/usr/bin/php7.4
 <?php
 
-chdir('/var/www/html/NBack/');
+chdir('/var/www/html/NBack');
 
 // Böngésző cash kezelő konstans.
 #define('RELOAD_INDICATOR'	, '' );
@@ -29,13 +30,126 @@ spl_autoload_register(function($className) {
 });
 
 
-function test(bool $stmt)
-{
-    $str = "Line:".debug_backtrace()[0]["line"]."-> ";
-    if ($stmt) {
-        $str.= "\e[0;30;42mtrue\e[0m";
+/**
+ * --------------------------------------------------------------
+ * tets Kiírja a függvény végrehajtásának várt és tényleges
+ * eredményét.
+ * --------------------------------------------------------------
+ * 
+ * @param object $obj A tesztelni kívánt osztály objektuma
+ * @param string $function A tesztelni kívánt osztály tagfüggvénye
+ * @param array $params A kapott függvény opcionális paraméterei
+ * @param bool $result A várt eredmény
+ * 
+ */
+function test(object $obj, string $function, ?array $params = [], bool $result = true)
+{    
+    // Bejövő adatok ellenőrzése
+    if (!(is_object($obj) && in_array($function, get_class_methods($obj))) ) {
+        echo "\e[1;37;41m".PHP_EOL.PHP_EOL."Callable should be a callable function, or a array with two members: \"0\" an object of a class,".PHP_EOL."and a \"1\", a member function of the \"0\" element on line ".debug_backtrace()[0]["line"]."!".PHP_EOL."\e[0m".PHP_EOL;
+        if (!is_object($obj)) {
+            echo "\e[1;37;41m".PHP_EOL.PHP_EOL."{$obj} is not a object!".PHP_EOL."\e[0m".PHP_EOL;
+        }
+        else {
+            echo "\e[1;37;41m".PHP_EOL.PHP_EOL."{$function} is not a member function of object (".get_class($obj).")!".PHP_EOL."\e[0m".PHP_EOL;
+        }
+        die;
+    }         
+    
+    // A hrtime az aktuális rendszeridőt adja vissza nanomásodpercben
+    $stime = hrtime(true);
+    $stmt = call_user_func([$obj, $function], ...$params); 
+    $dtime = (hrtime(true) - $stime) / 1000000.0;
+
+    // Ha a tesztelt osztálynak van szülője, vagyis tulajdonképpen a szülő
+    // osztályt teszteljük, akkor a szülő osztály nevén fog szerepelni a 
+    // generált mappa, különben magát a kapott osztályt teszteljük, így 
+    // annak a nevén kell szerepelnie.
+    $objName = get_parent_class($obj) ? get_parent_class($obj) : get_class($obj);
+    
+    $fileParams = tfile($dtime, $objName, $function);
+    
+    $str = sprintf("Line: %3d | avg: ", debug_backtrace()[0]["line"]);
+    // Ha a függvény végrehajtási idő magasabb 100 microsecundumnál, 
+    // piros kiemeléssel írja ki.
+
+    // Meghatározzuk, hogy a kívánt értéken felül van-e az átlag.
+    // Ha igen, piros kiemeléssel jelenítjük meg.
+    if ($fileParams["avg"] > 1) {
+        $str.=sprintf("\e[1;37;41m%5.3f\e[0m",$fileParams["avg"]);
     }
-    else
-        $str.= "\e[1;37;41mfalse\e[0m";
+    else {
+        $str.=sprintf("%5.3f",$fileParams["avg"]);
+    }
+
+    $str.=" ms items: ".$fileParams["count"];
+    $str .= "| act:";
+    $str .= sprintf("%7.3f ms", $dtime);
+    $str .= "| ";
+
+    // A kívánt és a kapott eredmény függvényében piros, vagy zöld
+    // háttérrel íratjuk ki az eredményt. Ha a függvény nem bool értékkel
+    // tér vissza, akkor azt írja ki az algoritmus.
+    if ($stmt == $result) {
+        if (gettype($stmt) == "boolean" || !$stmt)
+            $stmt = "true";        
+        $str.= "\e[0;30;42m{$stmt}\e[0m";
+    }
+    else {
+        if (gettype($stmt) == "boolean" || !$stmt)
+            $stmt = "false";
+        $str.= "\e[1;37;41m{$stmt}\e[0m";
+    }        
     echo $str.PHP_EOL;
+}
+
+/**
+ * --------------------------------------------------------------------------------
+ * Teszt fájlkezelő függvény. 
+ * A kapot osztály nevével létrehoz egy mappát a Test/statistics 
+ * almappában és abban hozza létre az egyes függvények futásidejének
+ * adatait tartalmazó fájlokat. A fájlok a függvény nevén szerepelnek.
+ * --------------------------------------------------------------------------------
+ * 
+ * @param float $dtime Differencia az algoritmus futása és vége közötti időpontokból
+ * @param string $objname   Az osztályhoz tartozó mappa nevéhez
+ * @param string $fname     A fájl a függvény nevén fog szerepelni.
+ * @return array            A kalkulált átlag "avg" és az esetek száma "count"
+ * @throws DomainException  A futtató felhasználónak nincs jogosultsága a fájlredszer művelethez.
+ * 
+ */
+function tfile(float $dtime, string $objName, string $fname): array
+{
+    $sum = 0;
+    $counter = 1;
+
+    $path = "Test/statistics/{$objName}/{$fname}.txt";
+
+    if (!is_dir("Test/statistics/{$objName}")) {
+        if (!mkdir("Test/statistics/{$objName}", 0775))
+            throw new DomainException("Can't create folder Test/statistics/{$objName}. Access denied for ".get_current_user());
+    }    
+
+    if (!is_file($path))
+        $resource = fopen($path,"x+");
+    else {
+        $resource = fopen($path,"r+");
+    }
+
+    if (!$resource)
+        throw new DomainException(
+            "Can't create file {$path}. Access denied for ".get_current_user());
+
+    while(!feof($resource)) {
+        $sum +=  (float)fgets($resource);
+        $counter++;
+    }
+
+    fputs($resource, $dtime.PHP_EOL);
+    fclose($resource);
+
+    return [
+        "avg" => round(($sum + $dtime) / ($counter + 1), 3), 
+        "count" => $counter + 1
+    ];
 }
